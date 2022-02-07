9cba9ebdceb30eb047752b59317f1c3324368929
camilaalmeidaa <99203530+camilaalmeidaa@users.noreply.github.com>
2022-02-07 14:18:35 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import exercicio1
+import exercicio2
+import exercicio3
+import exercicio4
+import exercicio5
+import exercicio6
+import exercicio7
+import exercicio8
+import exercicio9
+import exercicio10
+import exercicio11
+import exercicio12
+import exercicio13
+import exercicio14
+import exercicio15
+import exercicio16
+import exercicio17
+import exercicio18
+
+exercicio1.alo()
+exercicio2.numero()
+exercicio3.soma()
+exercicio4.media()
+exercicio5.converte()
+exercicio6.area()
+exercicio7.quadrado()
+exercicio8.salario()
+exercicio9.temp()
+exercicio10.temp2()
+exercicio11.operacoes()
+exercicio12.peso_ideal()
+exercicio13.peso_ideal2()
+exercicio14.multa()
+exercicio15.calcula_salario()
+exercicio16.tinta()
+exercicio17.tinta2()
+exercicio18.download()
